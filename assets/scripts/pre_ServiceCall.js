4c0b31ddbbdc32627a055b833f5a29b40ebafee2
dheeraj\Administrator <dheeraj.palusa@devrabbit.com>
2017-05-01 17:19:56 +0530

    no message

--- assets/scripts/pre_ServiceCall.js
+++ assets/scripts/pre_ServiceCall.js
@@ -1,11 +1,11 @@
 app.service('preService',function($http,httpService){
-       
+
       this.login=function(data){
         var url = "http://devrabbit.com/inspiring_wings/web_services/user_login.php";
         var headers={"Auth-Key":"55a2bc0181d79fd2db84d5e147698dc7"};
       return httpService.httpRequest(url, "P", data,headers)
     }
-     
+
         this.Eventget=function(data){
         var url = "http://devrabbit.com/inspiring_wings/web_services/events.php";
       var headers={"Auth-Key":"55a2bc0181d79fd2db84d5e147698dc7"};
@@ -16,7 +16,7 @@ app.service('preService',function($http,httpService){
       var headers={"Auth-Key":"55a2bc0181d79fd2db84d5e147698dc7"};
       return httpService.httpRequest(url, "P", data,headers)
     }
-    
+
         this.Storyget=function(data){
       var url = "http://devrabbit.com/inspiring_wings/web_services/stories.php";
       var headers={"Auth-Key":"55a2bc0181d79fd2db84d5e147698dc7"};
@@ -27,4 +27,19 @@ app.service('preService',function($http,httpService){
       var headers={"Auth-Key":"55a2bc0181d79fd2db84d5e147698dc7"};
       return httpService.httpRequest(url, "P", data,headers)
     }
+
+    this.allOpportunities=function(){
+      var url = "http://devrabbit.com/inspiring_wings/web_services/opportunities.php";
+      var headers={"Auth-Key":"55a2bc0181d79fd2db84d5e147698dc7"};
+      var data ={}
+    return httpService.httpRequest(url, "P", data,headers)
+
+}
+
+   this.addORUpdateOpp=function(data){
+     var url ="http://devrabbit.com/inspiring_wings/web_services/opportunities_action.php";
+     var headers={"Auth-Key":"55a2bc0181d79fd2db84d5e147698dc7"};
+   return httpService.httpRequest(url, "P", data,headers)
+
+}
   });
