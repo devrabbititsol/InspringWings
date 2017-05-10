0fc2278479ce41952d7cc11680fb2545944dc487
dheeraj\Administrator <dheeraj.palusa@devrabbit.com>
2017-05-10 17:20:18 +0530

    events filtering

--- assets/scripts/events_calender.js
+++ assets/scripts/events_calender.js
@@ -1,12 +1,25 @@
 app.controller('Events',function($rootScope,$scope,$localStorage,localData,preService,PaginationService){
     var data={};
+$scope.filter=false;
 $scope.pager={};
+$scope.pager1={};
 $scope.pageSize=6;
       preService.Eventget(data).then(function(res)
         {
             $scope.Events= res;
+            //alert(JSON.stringify($scope.Events));
+
+             var length = $scope.Events.length;
+           //alert($scope.Events[0].event_date);
+             for(i=0;i<length;i++){
+                 $scope.Events[i].event_date =  $scope.Events[i].event_date.slice(0,10);
+
+             }
              initController();
+          //   alert(JSON.stringify($scope.Events));
             $scope.convertToDate = function (stringDate){
+            //  alert("string");
+            //  alert(stringDate)
             var dateOut = new Date(stringDate);
             dateOut.setDate(dateOut.getDate());
             return dateOut;
@@ -18,25 +31,82 @@ $scope.pageSize=6;
            window.alert("err");
          });
      function initController() {
-                   // initialize to page 1
-              //     alert("init")
+              //alert("init");
                   $scope.setPage(1);
                }
 
                $scope.setPage=function(page) {
-                // alert("set");
+              //    alert("set")
                    if (page < 1 || page > $scope.pager.totalPages) {
                        return;
                    }
+            //       alert($scope.Events.length);
+              //     alert(page);
+            //       alert($scope.pageSize);
                    $scope.pager =PaginationService.pagination($scope.Events.length,page,$scope.pageSize);
-                   //alert(JSON.stringify($scope.pager));
+            //       alert("pager")
+            //        alert(JSON.stringify($scope.pager));
                     $scope.items = $scope.Events.slice($scope.pager.startIndex, $scope.pager.endIndex + 1);
+                    $scope.length=$scope.items.length;
+            //        alert("length");
+          //          alert($scope.length)
+                    if($scope.length >0){
+                      $scope.len = true;
+              //        alert($scope.len)
+                    }
+                    else{
+                      $scope.len = false;
+              //        alert($scope.len)
+                      $scope.pager.totalPages = 2;
+                    }
                    $scope.loading=false;
                }
-               
-        $scope.dateRangeFilter=function(startDate, endDate){
-           
-        $scope.fromdate1=startDate;
-        $scope.todate1=endDate;
+
+
+        $scope.search=function(startDate,endDate){
+      //    alert(startDate)
+    //      alert(endDate)
+    //      alert("search")
+           var calendar = new Array();
+
+           preService.Eventget(data).then(function(res)
+             {
+                 $scope.Events= res;
+          //
+                 var length = $scope.Events.length;
+          //
+                 for(i=0;i<length;i++){
+                     $scope.Events[i].event_date =  $scope.Events[i].event_date.slice(0,10);
+                       var x =moment($scope.Events[i].event_date);
+                    // //  alert(x);
+                    //  var s = moment(startDate, "YYYY-MM-dd");
+                    //  alert("s")
+                    //  alert(s);
+                    //   var e = moment(endDate, "YYYY-MM-dd");
+                    //   alert("e")
+                var s = moment(startDate);
+              //  alert("s")
+              //  alert(s);
+                var e = moment(endDate);
+              //  alert("e")
+              //    alert(e);
+                            if(x >=s  && x <= e){
+                              calendar.push($scope.Events[i]);
+                            }
+          //                  if (itemDate >= s && itemDate <= e)
+                }
+              //  alert(calendar.length);
+                $scope.Events=calendar
+              //    alert(JSON.stringify($scope.Events));
+                 initController();
+          //
+          //
+            },
+             function(err)
+            {
+                window.alert("err");
+              });
         }
+
+
 });
